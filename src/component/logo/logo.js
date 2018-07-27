02af05d1da49e6ed97eef06d430d6764e18a998f
jinfengjie <jinfengjie@weiboyi.com>
2018-07-27 09:16:26 +0800

    修改样式

--- src/component/logo/logo.js
+++ src/component/logo/logo.js
@@ -9,7 +9,7 @@ class Logo extends Component{
 	render() {
 		return (
 			<div className="logo_container">
-				<img src={logoImg} alt=""/>
+				<img src={logoImg} alt="" style={{width: '100%', height: '100%'}}/>
 			</div>
 		)
 	}
